b8285332a57886bcb758c5f0c70b256cfc606d94
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2016-05-01 08:10:29 -0500

    fancy loc wew

--- keymap.cc
+++ keymap.cc
@@ -1,4 +1,4 @@
-#include <vector>
+#include <map>
 #include "keymap.h"
 
 std::map<int, int> Keymap::keymap = init_map();
--- loc.sh
+++ loc.sh
@@ -1,1 +1,7 @@
 cat *.cc *.h | wc -l
+if [[ -n $1 ]] && [[ $1 = "-v" ]]
+then
+    for fn in *.cc *.h; do
+        wc -l $fn
+    done
+fi
