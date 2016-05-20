9ef147c96a0b326fcce3cbb4574a35c8d0774f22
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2016-05-20 18:09:20 -0500

    modify loc

--- loc.sh
+++ loc.sh
@@ -1,7 +1,7 @@
-cat src/*.cc src/*.h | wc -l
+cat src/* shaders/* | wc -l
 if [[ -n $1 ]] && [[ $1 = "-v" ]]
 then
-    for fn in src/*.cc src/*.h; do
+    for fn in src/* shaders/*; do
         wc -l $fn
     done
 fi
