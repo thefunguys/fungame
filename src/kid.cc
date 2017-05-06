f068a841268628d9d26feab065ad89013dade348
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2017-05-06 15:49:14 -0500

    fix kid following

--- src/kid.cc
+++ src/kid.cc
@@ -3,6 +3,8 @@
 #include "dialog.h"
 #include "kid.h"
 #include "fns.h"
+#include <glm/vec2.hpp>
+#include <glm/geometric.hpp>
 #include "game.h"
 
 Kid::Kid(std::string fname, int x, int y, int w, int h, int cw, int ch) :
@@ -12,11 +14,13 @@ Kid::Kid(std::string fname, int x, int y, int w, int h, int cw, int ch) :
 
 void Kid::update(double dt) {
     double dx = Game::p->pos.x + Game::p->w / 2 - (pos.x + w / 2);
-    double dy = Game::p->pos.y + Game::p->h - (pos.y + h / 2);
+    double dy = Game::p->pos.y + Game::p->h / 2 - (pos.y + h / 2);
+    auto dvec = glm::vec2(dx, dy);
+    auto dvecnorm = glm::normalize(dvec);
     rotation = atan(dy / dx) * RADDEG + 90;
-    if (fabs(dx) > 5.0 || fabs(dy) > 5.0) {
-        vel.x = 20.0 * dx / (fabs(dx) + 1);
-        vel.y = 20.0 * dy / (fabs(dy) + 1);
+    if (fabs(dx) > 10.0 || fabs(dy) > 10.0) {
+        vel.x = 20.0 * dvecnorm.x;
+        vel.y = 20.0 * dvecnorm.y;
     } else {
         vel.x = 0;
         vel.y = 0;
